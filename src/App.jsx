1027d1de3bebf214d7641a3de6f41cdd259dac24
Giovanna Fiuza <94703688+giifiuza@users.noreply.github.com>
2024-02-28 22:59:38 -0300

    header feito

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import Header from './components/Header'
 
 function App() {
 
   return (
-    <>
+    <div className="bg-black text-white min-h-screen">
       <Header />
-    </>
+    </div>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
-import React, {useState} from "react";
-import { useNavigate } from "react-router-dom"
+import React, { useState } from "react";
+import { HiMenu, HiX } from "react-icons/hi";
 import HeaderItem from "./HeaderItem";
+import Logo from '../assets/logo.svg';
 
 function Header() {
     const [toggle, setToggle] = useState(false);
@@ -14,43 +15,42 @@ function Header() {
         {
             title: "Team",
         },
-
     ];
 
     return (
-        <div className="flex items-center justify-between p-5 bg-slate-800 mb-[50px]">
-            <div className="flex  gap-8 items-center">
-                {/* <img
+        <div className="fixed top-0 justify-end left-0 w-full p-3 bg-black">
+            <div className="flex items-center justify-between p-5">
+                <img
                     src={Logo}
-                    className="w-[100px] 
-          md:w-[130px] object-cover"
-                /> */}
-                <div className="hidden content-center md:flex">
+                    className="w-[100px] md:w-[130px] object-cover"
+                />
+                <div className="hidden content-center gap-16 md:flex">
                     {menu.map((item) => (
-                        <HeaderItem name={item.title} description={item.description} />
+                        <HeaderItem key={item.title} name={item.title} />
                     ))}
                 </div>
-                <div className="flex md:hidden gap-5">
-                    {menu.map(
-                        (item, index) =>
-                            index < 3 && <HeaderItem name={""}  onClick={() => { navigate('/') }} />
-                    )}
-                    <div className="md:hidden" onClick={() => setToggle(!toggle)}>
-                        <HeaderItem name={""}  />
-                        {toggle ? (
-                            <div
-                                className="absolute mt-3 bg-[#000000] 
-              border-[1px] border-gray-700 p-3 px-5 py-4"
-                            >
-                                {menu.map(
-                                    (item, index) =>
-                                        index > 2 && (
-                                            <HeaderItem name={item.title} Icon={item.description} />
-                                        )
-                                )}
+                <div className="hidden md:block">
+                    <button className="justify-center">
+                        <div className="h-10 w-full rounded-md bg-gradient-to-r from-rose-800 to-blue-700 p-[1px]">
+                            <div className="flex h-full w-full rounded-md items-center justify-center p-2 px-7 bg-black">
+                                <h1 className="text-base font-semibold text-white">Access </h1>
                             </div>
-                        ) : null}
-                    </div>
+                        </div>
+                    </button>
+                </div>
+                <div className="flex md:hidden gap-5 items-center relative">
+                    {toggle ? (
+                        <div className="fixed top-0 right-0 h-full bg-[#000000] border-[1px] border-gray-700  p-3 px-5 py-4">
+                            <div className="flex justify-end">
+                                <HiX className="cursor-pointer" color="white" onClick={() => setToggle(false)} />
+                            </div>
+                            {menu.map((item) => (
+                                <HeaderItem key={item.title} name={item.title} onClick={() => {  }} />
+                            ))}
+                        </div>
+                    ) : (
+                        <HiMenu className="cursor-pointer" onClick={() => setToggle(true)} />
+                    )}
                 </div>
             </div>
         </div>
