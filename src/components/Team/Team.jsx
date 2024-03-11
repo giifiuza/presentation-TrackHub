a44ed9ef7e126ae38c9273413929cbb410bf064e
Fiuza Giovanna (CaP/ETS) <fig6ca@bosch.com>
2024-03-11 13:24:40 -0300

    pic teste - public

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -5,7 +5,7 @@ export default function Team() {
     const member = [
         {
             id:"01",
-            image:"bg-[url('./src/assets/diego.jpg')]",
+            image:"bg-[url('/diego.jpg')]",
             name:"Diego Alencar",
             position: "Fullstack Developer",
             github: "https://github.com/giifiuza/",
